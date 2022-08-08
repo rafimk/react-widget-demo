2be67bf4d6a9acd4a2b9ec27ca18e30be6eee9cc
Muhammed Rafi <m.rafi@afad.gov.ae>
2022-08-08 11:32:42 +0400

    Added aes encryption

--- src/components/CryptoList.jsx
+++ src/components/CryptoList.jsx
@@ -1,5 +1,31 @@
 import useCryptoData from "../hooks/useCryptoData";
 
+var aesjs = require('aes-js');
+
+var txtUserName = "testUser";  
+var txtpassword = "testPassword"; 
+
+var key = CryptoJS.enc.Utf8.parse('8080808080808080');  
+var iv = CryptoJS.enc.Utf8.parse('8080808080808080'); 
+
+var encryptedlogin = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(txtUserName), key,  
+{
+   keySize: 128 / 8,   
+   iv: iv,  
+   mode: CryptoJS.mode.CBC,  
+   padding: CryptoJS.pad.Pkcs7 
+}); 
+
+var encryptedpassword = CryptoJS.AES.encrypt(CryptoJS.enc.Utf8.parse(txtpassword), key,  
+{ 
+   keySize: 128 / 8,   
+   iv: iv,  
+   mode: CryptoJS.mode.CBC,  
+   padding: CryptoJS.pad.Pkcs7
+});  
+  
+
+
 const CryptoItem = (props) => (
   <div className="item">
     <img src={props.ImageUrl} className="icon" alt={props.Name} />
